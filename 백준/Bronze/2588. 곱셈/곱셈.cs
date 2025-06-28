1411e5eba993fec5e1dbba40fb40242dd2f4e65f
ForRest1217 <Jihan2519@naver.com>
2025-06-29 03:55:12 +0900

    [Bronze III] Title: 곱셈, Time: 44 ms, Memory: 5456 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cs
@@ -0,0 +1,6 @@
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(a * (b % 10));
+Console.WriteLine(a * ((b / 10) % 10));
+Console.WriteLine(a * (b / 100));
+Console.WriteLine(a * b);
