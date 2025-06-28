e116d0b322aaf51b2ebb26c60e9f74734797da2a
ForRest1217 <Jihan2519@naver.com>
2025-06-29 03:48:13 +0900

    [Bronze IV] Title: 숫자의 합, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cs
@@ -0,0 +1,10 @@
+int n = int.Parse(Console.ReadLine()); 
+string numbers = Console.ReadLine(); 
+
+int sum = 0;
+for (int i = 0; i < n; i++)
+{
+    sum += numbers[i] - '0';  
+}
+
+Console.WriteLine(sum);  
