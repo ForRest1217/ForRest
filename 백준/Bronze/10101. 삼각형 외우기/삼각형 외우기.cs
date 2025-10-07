a0603246e7b7c5452e560214745fe2ea1bfc1029
ForRest1217 <Jihan2519@naver.com>
2025-10-07 19:46:49 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 48 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.cs
@@ -0,0 +1,12 @@
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a + b + c != 180)
+    Console.WriteLine("Error");
+else if (a == b && b == c)
+    Console.WriteLine("Equilateral");
+else if (a == b || b == c || a == c)
+    Console.WriteLine("Isosceles");
+else
+    Console.WriteLine("Scalene");
