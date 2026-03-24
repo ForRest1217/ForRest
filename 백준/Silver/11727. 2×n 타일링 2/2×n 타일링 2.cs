8c8f5f340bc46f82759cff5f7f8d5d6be74b5ca4
ForRest1217 <Jihan2519@naver.com>
2026-03-25 08:48:56 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 52 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cs
@@ -0,0 +1,11 @@
+int a = int.Parse(Console.ReadLine());
+int[] b = new int[1001];
+b[1] = 1;
+b[2] = 3;
+
+for (int i = 3; i <= a; i++)
+{
+    b[i] = (b[i - 1] + 2 * b[i - 2]) % 10007;
+}
+
+Console.WriteLine(b[a]);
